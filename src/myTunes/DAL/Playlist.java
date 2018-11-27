d6f81416aef6257e611b184a3a423f2ac34da9ef
simge2204 <simgeer2204@msn.com>
2018-11-27 12:13:18 +0100

    playlist

---
+++ src/myTunes/DAL/Playlist.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myTunes.DAL;
+
+/**
+ *
+ * @author simge
+ */
+public class Playlist
+    {
+    //create a new playlist
+    public void createPlayList()
+            {
+                Playlist playList = new Playlist();
+            }
+    //edit the name of a playlist
+    //add a song to a playlist
+    public void addSong()
+        {
+        
+        }
+    //save playlist in database
+    //play a song in a playlist
+    public void playSong()
+        {
+        
+        }
+    //play next song in a playlist
+    public void playNextSong()
+        {
+        
+        }
+    //move a song in a playlist
+    public void moveSong()
+        {
+        
+        }
+    //delete a song from a playlist
+    public void deleteSong()
+        {
+        
+        }
+    //delete a playlist
+    public void deletePlayList()
+        {
+        
+        }
+    }
